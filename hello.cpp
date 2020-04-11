bc455ef872746be52f2fbe0ff34b031259ca6724
Simplesyd <60757006+Simplesyd@users.noreply.github.com>
2020-04-10 20:47:46 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+# include <cstdio> //this includes all hte libraries from cstdio
+# include <iostream> // includes a library for a large amount of IO capabilities 
+# include <string> // gives you the string data type and the functions that come along with it 
+
+using std::cout; // tells you when you call the cout function that you want to use the standard one
+using std::string; //tells it to use the standard string data type
+
+int main(int argc, char *argv[]){
+	
+	printf(" Hello World ") 
+	
+	return 0
+	
+}
